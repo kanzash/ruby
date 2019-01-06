a8bc7b254564621598c0d27f54b03e0e5fc462a3
kanzash <kanzash@github.com>
2019-01-05 21:12:20 -0500

    Example 42

---
+++ ex42.rb
@@ -0,0 +1,66 @@
+#LESSON 42: IS-A, HAS-A, OBJECTS AND CLASSES
+
+#Animal is -a object look at the extra credit
+
+class Animal
+end
+
+class Dog < Animal
+
+	def initialize(name)
+		@name = name
+	end
+end
+
+class Cat < Animal
+
+	def initialize(name)
+		@name = name
+	end
+end
+
+
+class Person
+
+	def initialize(name)
+		@name = name
+		@pet = nil
+	end
+
+	attr_accessor :pet
+end
+
+class Employee < Person
+	def initialize(name, salary)
+		super(name)
+		@salary = salary
+	end
+end
+
+class Fish
+end
+
+class Salmon < Fish
+end
+
+class Halibut < Fish
+end
+
+rover = Dog.new("Rover")
+
+satan = Cat.new("Satan")
+
+mary = Person.new("Mary")
+
+mary.pet = satan
+
+frank = Employee.new("Frank", 120000)
+
+frank.pet = rover
+
+flipper = Fish.new()
+
+crouse = Salmon.new()
+
+harry = Halibut.new()
+
